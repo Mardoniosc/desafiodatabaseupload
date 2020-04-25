19885cb2d30d94a05c0d4edc19f912a83648b7e0
Mardonio Costa <mardonio@live.com>
2020-04-25 12:48:40 -0300

    Finalizado 4 de 7

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,16 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transactionDelete = await transactionsRepository.delete(id);
+
+    if (!transactionDelete) {
+      throw new AppError('Error delete transaction', 401);
+    }
   }
 }
 
